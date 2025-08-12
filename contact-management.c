fb429317c630814046147d3204ce6f4278bab5a0
Tahsinul Mohsin <85359628+tahsinulmohsin@users.noreply.github.com>
2025-08-12 21:10:17 +0600

    Add files via upload
    This is the very first finalized draft of the project, containing the source code and the executable file (.exe). We will update the repository if the project is not approved. Feedbacks  are appreciated!

---
+++ contact-management.c
@@ -0,0 +1,129 @@
+/*Project: Contact Management System
+Group - 12
+
+Group members:
+Tahsinul Mohsin - 2524687642
+Mohammad Raihan Mustakim Zian - 2524666042*/
+
+#include<stdio.h>
+
+//declaring structures
+struct Contact
+{
+        char name[50];
+        char phone[50];
+        char email[50];
+}contact;
+
+//Function declaration
+void addContact(void);
+void displayContact(void);
+void menu(void);
+
+
+int main(void)
+{
+    menu();
+    return 0;
+}
+
+//Function Definition
+void addContact(void)
+{
+    //file creation with pointers
+    FILE *file;
+    file = fopen("contact-book.txt", "a" ); //appending data in the file
+    //control statement for file handling
+    if (file==NULL)
+    {
+        printf("SORRY! FILE DOES NOT EXIST OR COULD NOT BE CREATED!");
+    }
+
+    else
+    {
+        printf("The contact book is opened \n \n");
+
+        struct Contact contact;
+
+        printf("Enter Name: ");
+        fgets(contact.name, sizeof contact.name, stdin);
+
+        printf("Enter Phone: ");
+        fgets(contact.phone, sizeof contact.phone, stdin);
+
+        printf("Enter Email: ");
+        fgets(contact.email, sizeof contact.email, stdin);
+
+        //writing to the file
+        fprintf(file,"%s%s%s\n", contact.name, contact.phone, contact.email);
+        fclose(file);
+        printf("Contact succesfully added!! \n \n");
+    }
+}
+
+void displayContact(void)
+{
+
+
+    FILE *file;
+    file = fopen("contact-book.txt", "r"); //appending data in the file
+    //control statement for file handling
+    if (file==NULL)
+    {
+        printf("SORRY! FILE DOES NOT EXIST OR COULD NOT BE CREATED!");
+    }
+
+    else
+    {
+      printf("\nContacts: \n");
+
+      while(fscanf(file, " %[^\n] %[^\n] %[^\n]", contact.name, contact.phone, contact.email) != EOF)
+      {
+        printf("Name: %s\nPhone: %s\nEmail: %s\n\n\n", contact.name, contact.phone, contact.email);
+      }
+
+      fclose (file);
+
+    }
+}
+
+void menu (void)
+{
+    int choice;
+    do {
+
+    printf("Welcome to the Basic Contact Management System Software. \n");
+    printf("This program is developed by two noob freshers from the 252 batch at NSU!\n\n");
+    printf("Type the corresponding number in the menu to run your desired operation and press Enter\n\n");
+    printf("1. Add a Contact\n");
+    printf("2. Display contacts\n");
+    printf("3. Nah, I will rather exit this program\n\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 1:
+        addContact();
+        break;
+
+        case 2:
+        displayContact();
+        break;
+
+        case 3:
+        printf("Exiting the program. Good Bye!");
+        break;
+
+        default:
+        printf("INVALID CHOICE!!! Please try again! \n");
+    }
+
+} while (choice != 3);
+}
+
+
+
+
+
